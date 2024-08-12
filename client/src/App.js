e87f1632b7349653a7faad55695f1e2a65c897f0
Manasvinitroshan <manas.comet.25@gmail.com>
2024-08-12 14:16:44 -0500

    Started frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import styled, {ThemeProvider} from "@emotion/styled";
+import {darkTheme} from "./utils/Theme";
+import { Container } from "@mui/material";
+
 
 function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
+
+  return <ThemeProvider>
+    <Container>
+      <Wrapper>
+
+      </Wrapper>
+    </Container>
+  </ThemeProvider>
 }
 
 export default App;
